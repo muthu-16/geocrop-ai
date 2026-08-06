c9d1dfa0d754f4c75c611dd878e53cd2286f7486
Muthu B <muthu1979balu@gmail.com>
2026-08-06 12:19:06 +0530

    Add asset cache busting hashing to vite.config.js to force Vercel CDN fresh reload

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,14 @@ export default defineConfig({
         secure: false
       }
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].${Date.now()}.js`,
+        chunkFileNames: `assets/[name].${Date.now()}.js`,
+        assetFileNames: `assets/[name].${Date.now()}.[ext]`
+      }
+    }
   }
 })
